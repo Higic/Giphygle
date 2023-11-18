d1566c01bc4cbc6aa34e8610487c19463db8a37c
Higic <onni.alasaari@gmail.com>
2023-11-18 15:51:31 +0200

    added self modifyability for api keys

--- giphyproject/src/firebase.js
+++ giphyproject/src/firebase.js
@@ -3,17 +3,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAGqkj7dODESBdeGemmpSPug8Ak498xsog",
-    authDomain: "react-auth-d697d.firebaseapp.com",
-    projectId: "react-auth-d697d",
-    storageBucket: "react-auth-d697d.appspot.com",
-    messagingSenderId: "213907008419",
-    appId: "1:213907008419:web:152cb2d2d42036bd6a8816"
+    apiKey: "[INSERT API KEY HERE]",
+    authDomain: "[INSERT AUTH DOMAIN HERE]",
+    projectId: "[INSERT PROJECT ID HERE]",
+    storageBucket: "[INSERT STORAGE BUCKET HERE]",
+    messagingSenderId: "[INSERT MESSAGING SENDER ID HERE]",
+    appId: "[INSERT APP ID HERE]"
 };
 
 // Initialize Firebase
